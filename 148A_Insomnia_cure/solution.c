544bb02fb1bbff185ff7eaf31616620eb10247b6
Mayank Kumar <mayankkumar270607@gmail.com>
2026-02-09 15:29:25 +0530

    Add solution for 148A

---
+++ 148A_Insomnia_cure/solution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int k, l, m, n, d;
+    scanf("%d%d%d%d%d", &k, &l, &m, &n, &d);
+    int c = 0;
+    for (int i = 1; i <= d; i++) if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) c++;
+    printf("%d\n", c);
+    return 0;
+}
